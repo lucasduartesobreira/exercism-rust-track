563e12c6469dd76621745722badd577e66473f4f
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-08-02 00:13:39 -0300

    fix: Solution to Proverb challenge without side effects

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -3,20 +3,13 @@ pub fn build_proverb(list: &[&str]) -> String {
         return "".to_string();
     }
 
-    let mut proverb = String::from("");
-
-    let mut actual_word = list[0];
-    for &next_word in list[1..].iter() {
-        proverb.push_str(
-            format!(
+    list.iter()
+        .zip(list[1..].iter())
+        .fold("".to_string(), |acc, (&actual_word, &next_word)| {
+            acc + &format!(
                 "For want of a {} the {} was lost.\n",
                 actual_word, next_word
             )
-            .as_str(),
-        );
-        actual_word = next_word;
-    }
-    proverb.push_str(format!("And all for the want of a {}.", list[0]).as_str());
-
-    proverb
+        })
+        + &format!("And all for the want of a {}.", list[0])
 }
