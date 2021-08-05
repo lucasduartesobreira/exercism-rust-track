f7882eadbfca387a7e76f2010fe618368a3714be
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-08-04 23:54:24 -0300

    refactor: Made find_next_prime_factor more self-expressive with match

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,8 +1,4 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    if n == 1 {
-        return vec![];
-    }
-
     let mut factors = vec![];
     let mut n = n;
     let mut last_prime = 2;
@@ -17,17 +13,19 @@ pub fn factors(n: u64) -> Vec<u64> {
 }
 
 fn find_next_prime_factor(n: u64, last_prime: u64) -> u64 {
-    if n % last_prime == 0 {
-        return last_prime;
+    match (n, last_prime) {
+        (n, last_prime) if is_factor(n, last_prime) => last_prime,
+        (_, 2) => find_next_prime_factor(n, 3),
+        (n, last_prime) => {
+            let last_possible_prime = (n as f64).sqrt().ceil() as u64;
+            (last_prime..last_possible_prime)
+                .step_by(2)
+                .find(|&x| is_factor(n, x))
+                .unwrap_or(n)
+        }
     }
+}
 
-    if last_prime == 2 {
-        return find_next_prime_factor(n, 3);
-    }
-
-    let last_possible_prime = (n as f64).sqrt().ceil() as u64;
-    (last_prime..last_possible_prime)
-        .step_by(2)
-        .find(|x| n % x == 0)
-        .unwrap_or(n)
+fn is_factor(n: u64, factor: u64) -> bool {
+    n % factor == 0
 }
