c1089060f7360280ce05fe371725cbb3f9fd96fd
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-08-16 21:58:01 -0300

    Solution to the Hamming challenge

---
+++ hamming/src/lib.rs
@@ -0,0 +1,14 @@
+/// Return the Hamming distance between the strings,
+/// or None if the lengths are mismatched.
+pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
+    if s1.len() != s2.len() {
+        return None;
+    }
+
+    Some(
+        s1.chars()
+            .zip(s2.chars())
+            .filter(|(c1, c2)| c1 != c2)
+            .count(),
+    )
+}
