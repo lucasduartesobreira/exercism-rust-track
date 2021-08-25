7f6b717d5a3caadfd4f984f1e7cfa876f0ab6884
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-08-24 22:26:55 -0300

    Solution to the Scrabble Score challenge

---
+++ scrabble-score/src/lib.rs
@@ -0,0 +1,16 @@
+/// Compute the Scrabble score for a word.
+pub fn score(word: &str) -> u64 {
+    let word = word.to_ascii_uppercase();
+    word.chars().fold(0, |acc, ch| {
+        acc + match ch {
+            'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+            'D' | 'G' => 2,
+            'B' | 'C' | 'M' | 'P' => 3,
+            'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+            'K' => 5,
+            'J' | 'X' => 8,
+            'Q' | 'Z' => 10,
+            _ => 0,
+        }
+    })
+}
