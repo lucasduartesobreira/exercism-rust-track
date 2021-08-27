c873b71a926ca9b7078ec5903ae4e77519ee189e
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-08-26 21:57:35 -0300

    refactor(Protein Translation): of_rna now without side effects and more readable

--- protein-translation/src/lib.rs
+++ protein-translation/src/lib.rs
@@ -10,37 +10,13 @@ impl<'a> CodonsInfo<'a> {
     }
 
     pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
-        let codons = rna
-            .chars()
+        rna.chars()
             .collect::<Vec<char>>()
-            .windows(3)
-            .step_by(3)
-            .map(|x| x.iter().collect::<String>())
-            .collect::<Vec<String>>();
-
-        let mut codons_name = Vec::new();
-
-        let mut found_a_stop = false;
-        for codon in codons {
-            let codon = self.name_for(codon.as_str());
-            match codon {
-                Some(codon) => {
-                    if codon == "stop codon" {
-                        found_a_stop = true;
-                        break;
-                    }
-                    codons_name.push(codon);
-                }
-                None => return None,
-            }
-        }
-
-        // HACK: Find another way to deal with invalid lengths
-        if !found_a_stop && rna.len() % 3 != 0 {
-            return None;
-        }
-
-        Some(codons_name)
+            .chunks(3)
+            .map(|slice| slice.iter().collect::<String>())
+            .map(|codon| self.name_for(&codon))
+            .take_while(|&codon_name| codon_name != Some("stop codon"))
+            .collect()
     }
 }
 
