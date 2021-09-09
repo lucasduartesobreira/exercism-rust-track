d80e10fc85b78f8c8aba9df7c81b296da7d8fe97
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-09-08 23:45:09 -0300

    feat(Luhn): Solution to the Luhn challenge using iterators

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -10,54 +10,22 @@ pub fn is_valid(code: &str) -> bool {
         return false;
     }
 
-    let mut is_second = false;
-    let mut n_sum = 0;
+    code.chars()
+        .filter(|c| c != &' ')
+        .rev()
+        .map(|c| c.to_digit(10).unwrap())
+        .enumerate()
+        .map(|(index, number)| {
+            let multiplier = if index % 2 == 0 { 1 } else { 2 };
 
-    for c in code.chars().rev().filter(|c| c != &' ') {
-        let mut number = c.to_digit(10).unwrap();
+            let number = number * multiplier;
+            if number > 9 {
+                return number - 9;
+            }
 
-        if is_second {
-            number *= 2;
-        }
-
-        n_sum += number / 10;
-        n_sum += number % 10;
-
-        is_second = !is_second;
-    }
-
-    n_sum % 10 == 0
-
-    //code.chars()
-    //.filter(|c| c != &' ')
-    //.map(|c| {
-    //if c.is_ascii_digit() {
-    //c.to_digit(10)
-    //} else {
-    //None
-    //}
-    //})
-    //.enumerate()
-    //.try_fold(0u32, |acc, (index, option)| match option {
-    //Some(number) => {
-    //let multiple = if index % 2 == 0 { 2 } else { 1 };
-    //let number = number * multiple;
-
-    //let new_acc = acc + number / 10;
-    //let new_acc = new_acc + number % 10;
-
-    /*
-     *let multiplied = match number * multiple {
-     *    x if x > 9 => x - 9,
-     *    x => x,
-     *};
-     */
-
-    //Some(new_acc)
-    //}
-    //None => None,
-    //})
-    //.unwrap_or(1)
-    //% 10
-    //== 0
+            number
+        })
+        .sum::<u32>()
+        % 10
+        == 0
 }
