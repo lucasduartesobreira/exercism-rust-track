911f78de0845f341ae7baeca893a77e1412c6f84
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-09-09 23:19:15 -0300

    Trying a new way to solve the Bowling challenge

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -5,107 +5,83 @@ pub enum Error {
 }
 
 pub struct BowlingGame {
-    frames: Vec<Frame>,
-}
-
-struct Frame {
-    first_throw: u16,
-    second_throw: Option<u16>,
+    rolls: Vec<Option<u16>>,
+    max_rolls: usize,
 }
 
 impl BowlingGame {
     pub fn new() -> Self {
-        Self { frames: Vec::new() }
+        Self {
+            rolls: Vec::new(),
+            max_rolls: 21,
+        }
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        if self.frames.len() >= 10 {
+        if self.rolls.len() == 20 {
+            let last_roll = self.rolls.last().unwrap().unwrap_or_default();
+            let last_last_roll = self.rolls.get(19).unwrap().unwrap();
+
+            if last_roll + last_last_roll == 10 {
+                self.max_rolls = 22;
+            }
+        }
+
+        if self.rolls.len() >= self.max_rolls {
             return Err(Error::GameComplete);
         }
 
-        let last_frame = self.frames.last_mut();
-        match last_frame {
-            Some(last_frame) => {
-                if last_frame.has_a_second_shot_to_go() {
-                    last_frame.set_second_shot(pins)?;
-                } else {
-                    let new_frame = Frame::new(pins)?;
-                    self.frames.push(new_frame);
-                }
-
-                Ok(())
+        if self.rolls.len() % 2 != 0 || self.rolls.is_empty() {
+            if pins > 10 {
+                return Err(Error::NotEnoughPinsLeft);
             }
-            None => {
-                self.frames.push(Frame::new(pins)?);
-                Ok(())
+
+            self.rolls.push(Some(pins));
+
+            if pins == 10 {
+                self.rolls.push(None);
             }
-        }
-    }
+        } else {
+            let first_throw = self.rolls.last().unwrap();
 
-    pub fn score(&self) -> Option<u16> {
-        (0..self.frames.len()).try_fold(0, |acc, index| Some(acc + self.get_frame_score(index)?))
-    }
-
-    fn get_frame_score(&self, index: usize) -> Option<u16> {
-        let actual_frame = self.frames.get(index)?;
-
-        if index == 9 {
-            if actual_frame.is_an_spare() || actual_frame.is_an_strike() {
-                return Some(actual_frame.sum_pins() + self.frames[index + 1].sum_pins());
-            }
-            return Some(actual_frame.sum_pins());
-        }
-
-        if actual_frame.is_an_strike() {
-            let next_frame = self.frames.get(index + 1)?;
-
-            if next_frame.is_an_strike() {
-                return Some(20 + self.frames.get(index + 2)?.sum_pins());
+            if first_throw.unwrap() + pins <= 10 {
+                self.rolls.push(Some(pins));
+            } else {
+                return Err(Error::NotEnoughPinsLeft);
             }
         }
-
-        if actual_frame.is_an_spare() {
-            return Some(20 + self.frames.get(index + 1)?.first_throw);
-        }
-
-        Some(actual_frame.sum_pins())
-    }
-}
-
-impl Frame {
-    fn new(pins: u16) -> Result<Self, Error> {
-        if pins >= 10 {
-            return Err(Error::NotEnoughPinsLeft);
-        }
-
-        Ok(Self {
-            first_throw: pins,
-            second_throw: None,
-        })
-    }
-
-    fn has_a_second_shot_to_go(&self) -> bool {
-        self.second_throw.is_none() && self.first_throw != 10
-    }
-
-    fn set_second_shot(&mut self, pins: u16) -> Result<(), Error> {
-        if self.first_throw + pins >= 10 {
-            return Err(Error::NotEnoughPinsLeft);
-        }
-        self.second_throw = Some(pins);
 
         Ok(())
     }
 
-    fn is_an_strike(&self) -> bool {
-        self.first_throw == 10
+    pub fn score(&self) -> Option<u16> {
+        println!("{:?}", self.rolls);
+        let multiplier_iter =
+            self.rolls
+                .iter()
+                .enumerate()
+                .map(|(index, &opt_pins)| match opt_pins {
+                    Some(pins) => {
+                        if pins == 10 {
+                            let new_iter = self.rolls[index..].iter();
+                            return Some(
+                                new_iter
+                                    .filter(|x| x.is_none())
+                                    .enumerate()
+                                    .take_while(|&(index, _)| index < 3)
+                                    .fold(0, |acc, (index, sla)| acc + sla.unwrap()),
+                            );
+                        }
+                        None
+                    }
+                    None => None,
+                });
+        Some(0)
     }
+}
 
-    fn is_an_spare(&self) -> bool {
-        self.first_throw + self.second_throw.unwrap_or_default() == 10
-    }
-
-    fn sum_pins(&self) -> u16 {
-        self.first_throw + self.second_throw.unwrap_or_default()
+impl Default for BowlingGame {
+    fn default() -> Self {
+        Self::new()
     }
 }
