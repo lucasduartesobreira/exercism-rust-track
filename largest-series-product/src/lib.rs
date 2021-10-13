3840faa1f226519c8704043b1609973baa518aec
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-10-13 01:16:35 -0300

    refactor(Largest Series Product): Final solution to the challenge

--- largest-series-product/src/lib.rs
+++ largest-series-product/src/lib.rs
@@ -5,27 +5,29 @@ pub enum Error {
 }
 
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
-    if span > string_digits.len() {
-        return Err(Error::SpanTooLong);
-    }
-    if string_digits.is_empty() || span == 0 {
-        return Ok(1);
-    }
-    Ok(string_digits
-        .chars()
-        .collect::<Vec<_>>()
-        .windows(span)
-        .map(|chars| {
-            chars
-                .iter()
-                .map(|&c| c.to_digit(10).ok_or(Error::InvalidDigit(c)))
-                .collect::<Result<Vec<_>, _>>()
+    match (string_digits.len(), span) {
+        (x, y) if y > x => Err(Error::SpanTooLong),
+        (_, 0) => Ok(1),
+        _ => {
+            let normalized_vec_of_windows = string_digits
+                .chars()
+                .collect::<Vec<_>>()
+                .windows(span)
+                .map(|chars| {
+                    chars
+                        .iter()
+                        .map(|&c| c.to_digit(10).ok_or(Error::InvalidDigit(c)))
+                        .collect::<Result<Vec<_>, _>>()
+                })
+                .collect::<Result<Vec<_>, Error>>()?;
+
+            Ok(normalized_vec_of_windows
                 .into_iter()
-                .flat_map(|vec| vec.into_iter().map(u64::from))
-                .product()
-        })
-        .max()
-        .unwrap())
+                .map(|vecs| vecs.into_iter().map(u64::from).product())
+                .max()
+                .unwrap())
+        }
+    }
     /*
      *unimplemented!(
      *    "largest series product of a span of {} digits in {}",
