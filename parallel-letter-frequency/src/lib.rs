0b81a65be53ffbd3261f955dabd01a0b486fc29e
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-12-17 04:23:23 -0300

    feat(parallel-letter-frequency): another solution to challenge

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,12 +1,6 @@
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-    thread,
-};
+use std::{collections::HashMap, thread};
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let map: Arc<Mutex<HashMap<char, usize>>> = Arc::new(Mutex::new(HashMap::new()));
-
     let division = if worker_count < input.len() {
         (input.len() - 1) / worker_count
     } else {
@@ -15,35 +9,30 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
 
     let mut handles = vec![];
 
-    for sliced_input in input.chunks(division) {
-        let map = Arc::clone(&map);
-        let sla: Vec<String> = sliced_input.iter().map(|&s| s.to_string()).collect();
-        //let sla = sliced_input.to_owned();
+    for input in input.chunks(division) {
+        let input: Vec<String> = input.iter().map(|s| s.to_string()).collect();
         let handle = thread::spawn(move || {
-            sla.iter().for_each(|word| {
-                word.chars().filter(|c| c.is_alphabetic()).for_each(|ch| {
-                    *map.lock()
-                        .unwrap()
-                        .entry(ch.to_ascii_lowercase())
-                        .or_insert(0) += 1;
-                })
+            input.iter().fold(HashMap::new(), |map, word| {
+                word.chars()
+                    .filter(|letter| letter.is_alphabetic())
+                    .fold(map, |mut map, letter| {
+                        *map.entry(letter.to_ascii_lowercase()).or_insert(0) += 1;
+                        map
+                    })
             })
         });
 
         handles.push(handle);
     }
 
+    let mut map = HashMap::new();
+
     for handle in handles {
-        handle.join();
+        let result = handle.join().unwrap();
+        for (key, val) in result {
+            *map.entry(key).or_insert(0) += val;
+        }
     }
 
-    let x = map
-        .lock()
-        .unwrap()
-        .iter()
-        .map(|(key, value)| (*key, *value))
-        .collect();
-
-    println!("{:?}", x);
-    x
+    map
 }
