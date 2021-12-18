f190c922de56e85b14c28c7c451b3121bbc629e3
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-12-17 22:30:58 -0300

    perf(parallel-letter-frequecy): better perfomance by optimizing the worker_length

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,38 +1,42 @@
+use std::cmp::min;
 use std::{collections::HashMap, thread};
 
-pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let division = if worker_count < input.len() {
-        (input.len() - 1) / worker_count
+pub fn frequency(input: &[&'static str], worker_count: usize) -> HashMap<char, usize> {
+    let real_worker_count = min(input.len(), worker_count);
+    let worker_length = (input.len() / real_worker_count).max(1);
+    let fixed_worker_length = if real_worker_count * worker_length < input.len() {
+        worker_length + 1
     } else {
-        input.len()
+        worker_length
     };
 
-    let mut handles = vec![];
+    let mut thread_pool = Vec::with_capacity(real_worker_count);
 
-    for input in input.chunks(division) {
-        let input: Vec<String> = input.iter().map(|s| s.to_string()).collect();
-        let handle = thread::spawn(move || {
-            input.iter().fold(HashMap::new(), |map, word| {
-                word.chars()
-                    .filter(|letter| letter.is_alphabetic())
-                    .fold(map, |mut map, letter| {
-                        *map.entry(letter.to_ascii_lowercase()).or_insert(0) += 1;
-                        map
-                    })
-            })
-        });
-
-        handles.push(handle);
+    for input in input.chunks(fixed_worker_length) {
+        let owned_input = input.to_owned();
+        let my_thread = thread::spawn(move || count_letters(owned_input));
+        thread_pool.push(my_thread);
     }
 
-    let mut map = HashMap::new();
+    let mut map = HashMap::<char, usize>::new();
 
-    for handle in handles {
-        let result = handle.join().unwrap();
+    for my_thread in thread_pool {
+        let result = my_thread.join().unwrap();
         for (key, val) in result {
             *map.entry(key).or_insert(0) += val;
         }
     }
 
+    map
+}
+
+fn count_letters(input: Vec<&str>) -> HashMap<char, usize> {
+    let mut map = HashMap::<char, usize>::new();
+
+    for string in input {
+        for ch in string.chars() {
+            *map.entry(ch).or_default() += 1;
+        }
+    }
     map
 }
